f0f160745d6da0f56caac4df781fec9d7a75af39
Vache Apinyan <apinyan.v@gmail.com>
2023-03-22 23:09:54 +0400

    feat(gradeLabs): add file and read function gradeLabs for throw to error massage.

---
+++ errors/gradeLabs.js
@@ -0,0 +1,44 @@
+// Task 1.4
+// A teacher has created a gradeLabs function that verifies if student programming labs work. This function
+// loops over an array of JavaScript objects that should contain a student property and runLab property.
+// The runLab property is expected to be a function containing the student's code. The runLab function is
+// called and the result is compared to the expected result. If the result and expected results don't match,
+// then the lab is considered a failure.
+// The gradeLabs function works for the majority of cases. However, what happens if a student named their
+// function incorrectly? Run gradeLabs and pass it studentLabs as defined below
+// Upon running the second example, the teacher gets TypeError: lab.runLab is not a function.
+// Add a try/catch block inside of gradeLabs to catch an exception if the runLab property is not defined.
+// If the exception is thrown, the result should be set to the text "Error thrown".
+
+function gradeLabs(labs) {
+  for (let idx = 0; idx < labs.length; idx++) {
+    let lab = labs[idx];
+    try {
+      return `${lab.student} cod is worked: ${lab.runLab(5)}`;
+    } catch ({ message }) {
+      throw new Error("Error thrown");
+    }
+  }
+}
+let studentLabs = [
+  {
+    student: "Blake",
+    myCode: function (num) {
+      return Math.pow(num, num);
+    },
+  },
+  {
+    student: "Jessica",
+    runLab: function (num) {
+      return Math.pow(num, num);
+    },
+  },
+  {
+    student: "Mya",
+    runLab: function (num) {
+      return num * num;
+    },
+  },
+];
+
+console.log(gradeLabs(studentLabs));
