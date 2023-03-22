fa5a4c3ba4e405b7e06abe56b7f81dbd63fc0667
Vache Apinyan <apinyan.v@gmail.com>
2023-03-22 21:43:17 +0400

    feat(isPositive): add file and read function for positive number or throw to error massages.

---
+++ errors/isPositive.js
@@ -0,0 +1,19 @@
+// Task 1.2 // Complete the isPositive function below. It has one integer parameter a .
+// If the value of a is positive, it must return the string YES. Otherwise, it must throw an
+// Error according to the following rules:
+// 1. If a is 0, throw an Error with message = Zero Error.
+// 2. If a is negative, throw an Error with message = Negative Error.
+
+function isPositive (a) {
+    if (a > 0) {
+        return "YES";
+    }
+    if (a === 0) {
+        throw new Error("Zero Error");
+    }
+    if (a < 0) {
+        throw new Error("Negativ Error");
+    }
+}
+
+console.log(isPositive(0));
